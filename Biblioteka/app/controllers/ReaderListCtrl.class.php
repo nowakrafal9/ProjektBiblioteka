28a6d658541af5cbd2aae235114bba7a5b605688
Rafal <nowakr99@gmail.com>
2021-05-20 16:50:30 +0200

    Modified ReaderListCtrl.class.php

--- Biblioteka/app/controllers/ReaderListCtrl.class.php
+++ Biblioteka/app/controllers/ReaderListCtrl.class.php
@@ -15,27 +15,60 @@
         
         public function __construct() { $this->reader = new ReaderListForm(); }
         
-        public function validateList() {
-            // Get from HTML 
-                $this->reader->id_reader = ParamUtils::getFromRequest('id_reader');
-                $this->reader->name = ParamUtils::getFromRequest('name');
-                $this->reader->surname = ParamUtils::getFromRequest('surname');
+        public function getFormParam() {
+            $this->reader->id_reader = ParamUtils::getFromRequest('id_reader');
+            $this->reader->name = ParamUtils::getFromRequest('name');
+            $this->reader->surname = ParamUtils::getFromRequest('surname');
 
             return !App::getMessages()->isError();
         }   
         
-        public function validateInfo() {
-            // Get id from URL
-                $this->reader->id_reader = ParamUtils::getFromCleanURL(1, true, 'Błędne wywołanie aplikacji');
+        public function getFromUrl() {
+            $this->reader->id_reader = ParamUtils::getFromCleanURL(1, true, 'Błędne wywołanie aplikacji');
 
             return !App::getMessages()->isError();
         }   
+        
+        public function countRecords($table, &$where){
+            try {
+                $this->numRecords = App::getDB()->count($table, $where);
+            } catch (\PDOException $e) {
+                Utils::addErrorMessage('Wystąpił błąd podczas liczenia rekordów');
+                if (App::getConf()->debug){ Utils::addErrorMessage($e->getMessage()); }
+            }
+            
+            App::getSmarty()->assign('numRecords', $this->numRecords);
+        }
+        
+        public function prepareWhere($filter_params) {
+            $num_params = sizeof($filter_params);
+                
+            if ($num_params > 1) {
+                $where = ["AND" => &$filter_params];
+            } else {
+                $where = &$filter_params;
+            }
+            $where ["ORDER"] = ["surname","name"];
+            
+            return $where;
+        }
+        
+        public function getReaderInfo($param) {
+            try {
+                $this->records = App::getDB()->get("borrower_info", $param, ["id_borrower" => $this->reader->id_reader]);
+            } catch (\PDOException $e) {
+                Utils::addErrorMessage('Wystąpił błąd podczas pobierania rekordów');
+                if (App::getConf()->debug){ Utils::addErrorMessage($e->getMessage()); }
+            }    
+            
+            return $this->records;
+        }
         
         public function action_readerList(){
-            // Get params
-                $this->validateList();
+            # Get params
+                $this->getFormParam();
             
-            // Set filter params
+            # Set filter params
                 $filter_params = [];
                 if (isset($this->reader->id_reader) && strlen($this->reader->id_reader) > 0) {
                     $filter_params['id_borrower[~]'] = $this->reader->id_reader.'%';
@@ -46,17 +79,12 @@
                 if (isset($this->reader->surname) && strlen($this->reader->surname) > 0) {
                     $filter_params['surname[~]'] = $this->reader->surname.'%';
                 }
+                App::getSmarty()->assign('searchForm', $this->reader);
+                
+            # Prepare $where for DB operation
+                $where = $this->prepareWhere($filter_params);
             
-            // Prepare $where for DB operation
-                $num_params = sizeof($filter_params);
-                if ($num_params > 1) {
-                    $where = ["AND" => &$filter_params];
-                } else {
-                    $where = &$filter_params;
-                }
-                $where ["ORDER"] = ["surname","name"];
-            
-            // Get readers list
+            # Get readers list from DB
                 try {
                     $this->records = App::getDB()->select("borrower_info",
                         ["id_borrower",
@@ -65,58 +93,32 @@
                         $where);
                 } catch (\PDOException $e) {
                     Utils::addErrorMessage('Wystąpił błąd podczas pobierania rekordów');
-                    if (App::getConf()->debug)
-                        Utils::addErrorMessage($e->getMessage());
+                    if (App::getConf()->debug){ Utils::addErrorMessage($e->getMessage());}
                 }
                 App::getSmarty()->assign('records', $this->records);
                 
-            // Get number of readers registered
-                try {
-                    $this->numRecords = App::getDB()->count("borrower_info", $where);
-                } catch (\PDOException $e) {
-                    Utils::addErrorMessage('Wystąpił błąd podczas liczenia rekordów');
-                    if (App::getConf()->debug)
-                        Utils::addErrorMessage($e->getMessage());
-                }
-                App::getSmarty()->assign('numRecords', $this->numRecords);
+            # Get number of readers registered
+                $this->countRecords("borrower_info", $where); 
                 
-            // Send filter params to Smarty
-                App::getSmarty()->assign('searchForm', $this->reader);
-                 
-            // Get logged user name
-                App::getSmarty()->assign('user',unserialize($_SESSION['user'])); 
-            
-            // Redirect to page
+            # Redirect to page
                 App::getSmarty()->assign('pageMode',"readerList"); 
-                App::getSmarty()->display('Reader.tpl');
-        }
-        
-        public function getInfoRecords($param) {
-            try {
-                $this->records = App::getDB()->get("borrower_info", $param, ["id_borrower" => $this->reader->id_reader]);
-            } catch (\PDOException $e) {
-                Utils::addErrorMessage('Wystąpił błąd podczas pobierania rekordów');
-                if (App::getConf()->debug)
-                    Utils::addErrorMessage($e->getMessage());
-            }    
-            
-            return $this->records;
+                $this->generateView();
         }
         
         public function action_readerInfo(){
-            // Get params
-                $this->validateInfo();
+            # Get params
+                $this->getFromUrl();
             
-            // Get reader personal info        
-                App::getSmarty()->assign('name', $this->getInfoRecords("name"));
-                App::getSmarty()->assign('surname', $this->getInfoRecords("surname"));
-                App::getSmarty()->assign('city', $this->getInfoRecords("city"));
-                App::getSmarty()->assign('address', $this->getInfoRecords("address"));
-                App::getSmarty()->assign('postal_code', $this->getInfoRecords("postal_code"));
-                App::getSmarty()->assign('phone_number', $this->getInfoRecords("phone_number"));
-                App::getSmarty()->assign('email', $this->getInfoRecords("email"));
+            # Get reader personal info        
+                App::getSmarty()->assign('name', $this->getReaderInfo("name"));
+                App::getSmarty()->assign('surname', $this->getReaderInfo("surname"));
+                App::getSmarty()->assign('city', $this->getReaderInfo("city"));
+                App::getSmarty()->assign('address', $this->getReaderInfo("address"));
+                App::getSmarty()->assign('postal_code', $this->getReaderInfo("postal_code"));
+                App::getSmarty()->assign('phone_number', $this->getReaderInfo("phone_number"));
+                App::getSmarty()->assign('email', $this->getReaderInfo("email"));
                 
-            // Get borrowed books by reader
+            # Get borrowed books by reader
                 try {
                     $this->records = App::getDB()->select("borrowed_books", 
                         ["[><]book_stock" => ["id_book" => "id_book"]],
@@ -127,29 +129,23 @@
                         ["id_borrower" => $this->reader->id_reader]);
                 } catch (\PDOException $e) {
                     Utils::addErrorMessage('Wystąpił błąd podczas pobierania rekordów');
-                    if (App::getConf()->debug)
-                        Utils::addErrorMessage($e->getMessage());
+                    if (App::getConf()->debug){ Utils::addErrorMessage($e->getMessage()); }
                 }    
                 App::getSmarty()->assign('records', $this->records);   
             
-            // Get number of books borrowed by reader
-                try {
-                    $this->numRecords = App::getDB()->count("borrowed_books", ["id_borrower" => $this->reader->id_reader]);
-                } catch (\PDOException $e) {
-                    Utils::addErrorMessage('Wystąpił błąd podczas liczenia rekordów');
-                    if (App::getConf()->debug)
-                        Utils::addErrorMessage($e->getMessage());
-                }
-                App::getSmarty()->assign('numRecords', $this->numRecords);
+            # Get number of books borrowed by reader
+                $where["id_borrower~"] = $this->reader->id_reader;
+                $this->countRecords("borrowed_books", $where); 
             
-            // Get today date
+            # Redirect to page
                 App::getSmarty()->assign('dateToday', date("Y-m-d"));
-            
-            // Get logged user name
-                App::getSmarty()->assign('user',unserialize($_SESSION['user'])); 
-            
-            // Redirect to page
                 App::getSmarty()->assign('pageMode',"readerInfo"); 
-                App::getSmarty()->display('Reader.tpl');
+                
+                $this->generateView();
+        }
+        
+        public function generateView(){
+            App::getSmarty()->assign('user',unserialize($_SESSION['user'])); 
+            App::getSmarty()->display('Reader.tpl');
         }
     }
