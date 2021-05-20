e1179674de4678321e562f25fc8998f26e777267
Rafal <nowakr99@gmail.com>
2021-05-20 13:53:23 +0200

    User.class.php

--- Biblioteka/app/transfer/User.class.php
+++ Biblioteka/app/transfer/User.class.php
@@ -4,9 +4,11 @@
     class User {
         public $login;
         public $role;
+        public $id_employee;
         
-        public function __construct($login, $role) {
+        public function __construct($login, $role, $id_employee) {
             $this->login = $login;
             $this->role = $role;
+            $this->id_employee = $id_employee;
         }
     }
