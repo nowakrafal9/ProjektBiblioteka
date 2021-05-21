30d3540c7e01b49a4c2d9ff8596f2ad270190ac4
Rafal <nowakr99@gmail.com>
2021-05-21 11:57:14 +0200

    Modified routing.php

--- Biblioteka/routing.php
+++ Biblioteka/routing.php
@@ -1,24 +1,24 @@
 <?php
+    use core\App;
+    use core\Utils;
 
-use core\App;
-use core\Utils;
+    App::getRouter()->setDefaultRoute('main'); #default action
+    App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
 
-App::getRouter()->setDefaultRoute('main'); #default action
-App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
+    Utils::addRoute('main', 'MainCtrl', ['Pracownik', "Administrator"]);
 
-Utils::addRoute('main', 'MainCtrl', ['Pracownik', "Administrator"]);
+    Utils::addRoute('login', 'LoginCtrl');
+    Utils::addRoute('logout', 'LoginCtrl');
 
-Utils::addRoute('login', 'LoginCtrl');
-Utils::addRoute('logout', 'LoginCtrl');
+    Utils::addRoute('readerList', 'ReaderCtrl', ['Pracownik', "Administrator"]);
+    Utils::addRoute('readerInfo', 'ReaderCtrl', ['Pracownik', "Administrator"]);
 
-Utils::addRoute('readerList', 'ReaderCtrl', ['Pracownik', "Administrator"]);
-Utils::addRoute('readerInfo', 'ReaderCtrl', ['Pracownik', "Administrator"]);
+    Utils::addRoute('bookList', 'BookCtrl', ['Pracownik', "Administrator"]);
+    Utils::addRoute('bookInfo', 'BookCtrl', ['Pracownik', "Administrator"]);
+    Utils::addRoute('bookStock', 'BookCtrl', ['Pracownik', "Administrator"]);
 
-Utils::addRoute('bookList', 'BookCtrl', ['Pracownik', "Administrator"]);
-Utils::addRoute('bookInfo', 'BookCtrl', ['Pracownik', "Administrator"]);
-Utils::addRoute('bookStock', 'BookCtrl', ['Pracownik', "Administrator"]);
+    Utils::addRoute('borrowedList', 'BorrowedBooksCtrl', ['Pracownik', "Administrator"]);
+    Utils::addRoute('borrowedReturn', 'BorrowedBooksCtrl', ['Pracownik', "Administrator"]);
 
-Utils::addRoute('borrowedList', 'BorrowedBooksCtrl', ['Pracownik', "Administrator"]);
-Utils::addRoute('borrowedReturn', 'BorrowedBooksCtrl', ['Pracownik', "Administrator"]);
-Utils::addRoute('bookBorrow', 'BorrowedBooksCtrl', ['Pracownik', "Administrator"]);
-Utils::addRoute('bookReturn', 'BorrowedBooksCtrl', ['Pracownik', "Administrator"]);
+    Utils::addRoute('bookReturn', 'BorrowedBooksCtrl', ['Pracownik', "Administrator"]);
+    Utils::addRoute('bookBorrow', 'BorrowBookCtrl', ['Pracownik', "Administrator"]);
