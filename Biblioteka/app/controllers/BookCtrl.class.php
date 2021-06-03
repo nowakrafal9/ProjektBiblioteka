b9c58e7253a3b8c9d28119a8ef58b06a2360a723
Rafal <nowakr99@gmail.com>
2021-06-03 19:11:40 +0200

    Modified files - repaired form for book stock

--- Biblioteka/app/controllers/BookCtrl.class.php
+++ Biblioteka/app/controllers/BookCtrl.class.php
@@ -90,18 +90,18 @@
             # Set filter params    
                 $filter_params = [];
                 if (isset($this->book->book_code) && strlen($this->book->book_code) > 0) {
-                    $filter_params['id_book[~]'] = $this->book->book_code.'%';
+                    $filter_params['book_stock.id_book[~]'] = $this->book->book_code.'%';
                 }
                 if (isset($this->book->title) && strlen($this->book->title) > 0) {
-                    $filter_params['title[~]'] = $this->book->title.'%';
+                    $filter_params['book_stock.title[~]'] = $this->book->title.'%';
                 }
                 if (isset($this->book->borrowed) && strlen($this->book->borrowed) > 0) {
-                    $filter_params['borrowed[~]'] = $this->book->borrowed.'%';
+                    $filter_params['book_stock.borrowed[~]'] = $this->book->borrowed.'%';
                 }
                 App::getSmarty()->assign('searchForm', $this->book);
 
             # Prepare $where for DB operation
-                $order = ["id_book"];
+                $order = ["book_stock.id_book"];
                 $where = FunctionsDB::prepareWhere($filter_params, $order);          
 
             # Get books in library from DB
@@ -109,7 +109,6 @@
                 $column = ["book_stock.id_book", "book_info.title", "book_stock.borrowed"];
 
                 App::getSmarty()->assign('records', FunctionsDB::getRecords("select", "book_stock", $join, $column, $where));
-
             # Get number of books in library
                 FunctionsDB::countRecords("book_stock", $where); 
 
